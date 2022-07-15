de628bd12467e3d83909a9ec4bf8b31b463ba7b5
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-07-15 14:07:56 +0530

    15/07/2022

---
+++ middleware.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+    console.log('Middleware executed');
+    next();
+});
+
+app.get('/', function(req, res, next) {
+    console.log('Welcome');
+    res.send('Hello World');
+    next();
+});
+
+app.use('/', function(req, res, next) {
+    console.log('Middleware ended');
+});
+
+app.listen(8002);
